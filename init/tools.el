a004cc32a20fe01709d15695474e32375cf9d863
accidentalrebel <accidentalrebel@gmail.com>
2017-03-29 12:05:00 +0800

    Added more snippets for 2DKit development.

--- init/tools.el
+++ init/tools.el
@@ -135,9 +135,7 @@
 
 (use-package avy
   :bind ( :map arebel-custom-key-map
-	       ("C-'" . avy-goto-char-1)
-	       ;; ("C-; C-;" . avy-goto-char-2)
-	       ;; ("C-. ," . avy-goto-word-0)
+	       ("C-'" . avy-goto-char-2)
 	       ("C-;" . avy-goto-word-1)
 	       ("C-," . avy-goto-line)))
 
