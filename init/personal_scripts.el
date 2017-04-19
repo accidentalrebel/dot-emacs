e625e448e60d81b94ac4403502de01e1f9085a02
accidentalrebel <accidentalrebel@gmail.com>
2017-04-19 12:55:36 +0800

    Added the code for converting org-journal to org-page.

--- init/personal_scripts.el
+++ init/personal_scripts.el
@@ -10,6 +10,17 @@
   (interactive)
   (start-process "putty" nil "putty.exe" "-load" "LinodeARebel" user--linode-box-connect-url))
 
+(defun arebel-org-journal-entry-to-org-page-post ()
+  "Copy the org-journal entry at point and then convert it to a org-page new post buffer."
+  (interactive)
+  (if (eq 'org-journal-mode major-mode)
+      (let ((headline-text (nth 4 (org-heading-components)))
+	    (entry-text (org-get-entry)))
+	(funcall-interactively 'op/new-post "blog" (concat (buffer-name) "-" headline-text))
+	(goto-char (point-max))
+	(insert entry-text))
+    (message "This function can only be called inside org-journal-mode.")) )
+
 (defun arebel-blog-commit ()
   (interactive)
   (let ((blog-path (concat user--linux-dropbox-folder "orgmode/blog")))
